109e7e2bc4fa8f2ad1006a43135a45de0514ad25
CJ\agirbur <albgirbur@gmail.com>
2023-11-17 08:48:59 +0100

    imc

---
+++ imc/actalt.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route("/", methods=["POST","GET"])
+def hola():
+    peso = 0
+    altura = 0
+    imc = 0
+    if (request.method == "POST"):
+        peso = float(request.form.get("peso"))
+        altura = float(request.form.get("altura"))
+        imc = (peso/(altura*altura))
+        print(peso, altura, imc)
+    if imc < 18.5:
+        mensaje = "Peso bajo"
+    elif imc > 18.5 and imc < 24.9:
+        mensaje = "Peso normal"
+    elif imc > 24.9 and imc < 29.9:
+        mensaje = "Sobrepeso"
+    elif imc > 30.0:
+        mensaje = "Obesidad"
+    return render_template("index.html", peso=peso, altura=altura, imc=imc, mensaje=mensaje)
+
+app.run()
